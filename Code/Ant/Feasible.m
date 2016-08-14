45ab08b09fdf18ed7687ae6c5be8800939e37c37
Maziyar Gerami <maziyar.grami@gmail.com>
2016-08-14 09:51:09 +0330

    Commit for /d/Work/Thesis/Elham Ghasemi/Code/Ant/Feasible.m

---
+++ Code/Ant/Feasible.m
@@ -0,0 +1,28 @@
+function [ result ] = Feasible( Position,nVms, VMs, CreatedVMs, nServer, serverProcessor, serverRam, serverBandwidth )
+%ISFEASIBLE Summary of this function goes here
+%   Detailed explanation goes here
+server = zeros (nServer,3);
+result = true;
+
+for i=1:length(Position)
+   
+    server(Position(i),1) = server(Position(i),1) + VMs.Cores(CreatedVMs(i));
+    server(Position(i),2) = server(Position(i),2) + VMs.Ram(CreatedVMs(i));
+    server(Position(i),3) = server(Position(i),3) + VMs.Bandwidth(CreatedVMs(i));
+    
+
+end
+
+for i=1:length(server)
+    
+    if server(i,1)>serverProcessor || server(i,2) > serverRam || server(i,3) >serverBandwidth
+   
+        result = false;
+        break;
+        
+    end
+    
+end
+
+
+
